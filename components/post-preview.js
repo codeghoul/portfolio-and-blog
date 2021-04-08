2e0965fe6614aa52337fa20db102804b2d2cdb1c
codeghoul <jayesh.patel.2197@gmail.com>
2021-04-08 20:06:48 +0530

    Fix Slug URIs.

--- components/post-preview.js
+++ components/post-preview.js
@@ -13,18 +13,18 @@ export default function PostPreview({
 }) {
   return (
     <div>
-      <div className="mb-5">
+      <div className='mb-5'>
         <CoverImage title={title} slug={slug} url={coverImage.url} />
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a className="hover:underline">{title}</a>
+      <h3 className='text-3xl mb-3 leading-snug'>
+        <Link as={`/blogs/${slug}`} href='/blogs/[slug]'>
+          <a className='hover:underline'>{title}</a>
         </Link>
       </h3>
-      <div className="text-lg mb-4">
+      <div className='text-lg mb-4'>
         <DateComponent dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+      <p className='text-lg leading-relaxed mb-4'>{excerpt}</p>
       {author && <Avatar name={author.name} picture={author.picture} />}
     </div>
   )
