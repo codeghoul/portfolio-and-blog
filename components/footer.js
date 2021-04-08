984e31a55b9e2e8e7b78df1eae3b257cbcfaedaa
codeghoul <jayesh.patel.2197@gmail.com>
2021-04-08 19:30:11 +0530

    UI fixes.

--- components/footer.js
+++ components/footer.js
@@ -2,9 +2,9 @@ import Container from './container'
 
 export default function Footer() {
   return (
-    <footer className='bg-accent-1 border-t border-accent-2 mb-8 mt-8 '>
+    <footer className='bg-accent-1 border-t border-accent-2'>
       <Container>
-        <p className='text-xl font-bold tracking-tight md:tracking-tighter leading-tight text-center'>
+        <p className='text-xl font-bold tracking-tight md:tracking-tighter leading-tight text-center mb-4 mt-4'>
           Made with ❤ and ☕ by Jayesh Patel
         </p>
       </Container>
