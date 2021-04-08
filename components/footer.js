dde45092f44fa5cfe5738a10d13b600be87bec9b
codeghoul <jayesh.patel.2197@gmail.com>
2021-04-08 19:18:11 +0530

    Improve Landing Page.

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className='bg-accent-1 border-t border-accent-2 mb-8 mt-8 '>
       <Container>
         <p className='text-xl font-bold tracking-tight md:tracking-tighter leading-tight text-center'>
-          I made it.
+          Made with ❤ and ☕ by Jayesh Patel
         </p>
       </Container>
     </footer>
--- pages/index.js
+++ pages/index.js
@@ -7,29 +7,37 @@ export default function HomePage() {
     <>
       <Layout preview={false}>
         <Head>
-          <title>Welcome.</title>
+          <title>Welcome to my Website!</title>
         </Head>
-        <div className='mt-28 md:mt-56'>
-          <h1 className='text-3xl font-extrabold'>Hi! I'm Jayesh!</h1>
-          <br />
-          <p className='text-left leading-tight '>
-            For the past 1 year, I’ve been working as a Backend Developer at
-            GlobalLogic Inc. I have a sound knowledge of Microservice
-            Architecture & Spring Framework is my weapon of choice though I do
-            have some background in Frontend Development and I love dabbling in
-            React.js & Gatsby during my free time.
-          </p>
-          <br />
-          <p className='text-left leading-tight'>
-            I’ve always been good at problem-solving & building new things.
-            Being a passionate learner, I look forward to expanding my
-            experience across different industries & technologies.
-          </p>
-        </div>
-        <br />
-        <Link href='/posts'>
-          <a className='underline italic'>Look at my blog posts!</a>
-        </Link>
+        <section className='text-gray-600'>
+          <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
+            <img
+              className='lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded'
+              alt='hero'
+              src='https://dummyimage.com/720x600'
+            />
+            <div className='text-center lg:w-2/3 w-full'>
+              <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900'>
+                Hi! I'm Jayesh!
+              </h1>
+              <p className='mb-8 leading-relaxed'>
+                For the past 1 year, I’ve been working as a Backend Developer at
+                GlobalLogic Inc. I have a sound knowledge of Microservice
+                Architecture & Spring Framework is my weapon of choice though I
+                do have some background in Frontend Development and I love
+                dabbling in React.js & Gatsby during my free time.
+              </p>
+              <p className='mb-8 leading-relaxed'>
+                I’ve always been good at problem-solving & building new things.
+                Being a passionate learner, I look forward to expanding my
+                experience across different industries & technologies.
+              </p>
+            </div>
+            <Link href='/posts'>
+              <a className='underline italic'>Checkout my blog posts!</a>
+            </Link>
+          </div>
+        </section>
       </Layout>
     </>
   )
