ee46b26677a1533709d4c0ccccaa53a7e6474eff
codeghoul <jayesh.patel.2197@gmail.com>
2021-04-09 14:19:50 +0530

    Fix Favicon not showing and Remove unused headers.

--- components/header.js
+++ components/header.js
@@ -1,8 +1,10 @@
 import Link from 'next/link'
+import Meta from './meta'
 
 export default function Header() {
   return (
     <header>
+      <Meta />
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
         <Link href='/'>
           <a className='hover:underline text-2xl font-extrabold text-black'>
@@ -10,15 +12,15 @@ export default function Header() {
           </a>
         </Link>
         <nav className='md:ml-auto flex flex-wrap items-center text-base justify-center text-gray-600'>
-          <Link href='/'>
-            <a className='mr-5 hover:text-gray-900'>About</a>
-          </Link>
+          {/* <Link href='/'>
+            <a className='mx-2 hover:text-gray-900'>About</a>
+          </Link> */}
           <Link href='/blogs'>
-            <a className='mr-5 hover:text-gray-900'>Blogs</a>
+            <a className='mx-2 hover:text-gray-900'>Blogs</a>
           </Link>
-          <Link href='/'>
-            <a className='mr-5 hover:text-gray-900'>Projects</a>
-          </Link>
+          {/* <Link href='/'>
+            <a className='mx-2 hover:text-gray-900'>Projects</a>
+          </Link> */}
         </nav>
       </div>
     </header>
--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,10 @@
 import Document, { Html, Main, Head, NextScript } from 'next/document'
-import Meta from '../components/meta'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
         <Head />
-        <Meta />
         <body>
           <Main />
           <NextScript />
