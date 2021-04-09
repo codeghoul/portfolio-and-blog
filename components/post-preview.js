e4f2b94ef5ad8e8fd2aedd0f9fcbd05477c7cf14
codeghoul <jayesh.patel.2197@gmail.com>
2021-04-09 17:22:29 +0530

    Preview UI update.

--- components/post-preview.js
+++ components/post-preview.js
@@ -11,21 +11,39 @@ export default function PostPreview({
   author,
   slug,
 }) {
+  const image = (
+    <img
+      className='lg:h-full md:h-36 w-full object-cover object-center rounded-lg rounded-b-none'
+      src={coverImage.url}
+      alt={`Cover Image for ${title}`}
+    />
+  )
+
   return (
-    <div className='p-5'>
+    <div className='m-5 shadow-lg hover:shadow-xl rounded-lg'>
       <div className='mb-5'>
-        <CoverImage title={title} slug={slug} url={coverImage.url} />
+        {slug ? (
+          <Link as={`/blogs/${slug}`} href='/blogs/[slug]'>
+            <a aria-label={title}>{image}</a>
+          </Link>
+        ) : (
+          image
+        )}
       </div>
-      <h3 className='text-3xl mb-3 leading-snug'>
-        <Link as={`/blogs/${slug}`} href='/blogs/[slug]'>
-          <a className='hover:underline'>{title}</a>
-        </Link>
-      </h3>
-      <div className='text-lg mb-4'>
-        <DateComponent dateString={date} />
+      <div className='p-5'>
+        <h3 className='text-xl lg:text-3xl mb-3 leading-snug'>
+          <Link as={`/blogs/${slug}`} href='/blogs/[slug]'>
+            <a className='hover:underline'>{title}</a>
+          </Link>
+        </h3>
+        <div className='text-gray-600 text:sm lg:text-lg mb-4'>
+          <DateComponent dateString={date} />
+        </div>
+        <p className='text-md lg:text-lg leading-relaxed mb-4 line-clamp-2 lg:line-clamp-3'>
+          {excerpt}
+        </p>
+        {author && <Avatar name={author.name} picture={author.picture} />}
       </div>
-      <p className='text-lg leading-relaxed mb-4'>{excerpt}</p>
-      {author && <Avatar name={author.name} picture={author.picture} />}
     </div>
   )
 }
