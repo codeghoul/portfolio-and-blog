0c17bd32e2b0241516f7f417da5057dd3109a569
codeghoul <jayesh.patel.2197@gmail.com>
2021-04-09 18:00:08 +0530

    A better blog page.

--- components/post-body.js
+++ components/post-body.js
@@ -1,8 +1,8 @@
 export default function PostBody({ content }) {
   return (
-    <div className='max-w-2xl mx-auto'>
+    <div className='w-11/12 md:w-8/12 mx-auto'>
       <div
-        className='prose lg:prose-xl'
+        className='prose lg:prose-xl max-w-none'
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
--- components/post-header.js
+++ components/post-header.js
@@ -5,22 +5,17 @@ import PostTitle from '../components/post-title'
 
 export default function PostHeader({ title, coverImage, date, author }) {
   return (
-    <>
-      <PostTitle>{title}</PostTitle>
-      <div className='hidden md:block md:mb-12'>
-        {author && <Avatar name={author.name} picture={author.picture} />}
-      </div>
-      <div className='mb-8 md:mb-16 sm:mx-0'>
+    <div className='w-11/12 md:w-8/12 mx-auto'>
+      <div className='mb-6 md:mb-14 sm:mx-0'>
         <CoverImage title={title} url={coverImage.url} />
       </div>
-      <div className='max-w-2xl mx-auto'>
-        <div className='block md:hidden mb-6'>
-          {author && <Avatar name={author.name} picture={author.picture} />}
-        </div>
-        <div className='mb-6 text-lg'>
+      <PostTitle>{title}</PostTitle>
+      <div className='flex flex-row justify-between items-center mb-6'>
+        {author && <Avatar name={author.name} picture={author.picture} />}
+        <div className='text-xs lg:text-lg text-gray-600'>
           <DateComponent dateString={date} />
         </div>
       </div>
-    </>
+    </div>
   )
 }
