552bc39c2964409a5e39801dcab45fd2d8a12086
codeghoul <jayesh.patel.2197@gmail.com>
2021-04-10 13:04:11 +0530

    Fix Accessibility Issue.

--- components/meta.js
+++ components/meta.js
@@ -35,10 +35,7 @@ export default function Meta() {
       <meta name='theme-color' content='#000' />
       <meta charSet='utf-8' />
       <meta name='robots' content='index, follow' />
-      <meta
-        name='viewport'
-        content='width=device-width, initial-scale=1.0, maximum-scale=1'
-      />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <meta
         name='keywords'
         content='jayesh, patel, backend, developer, research, engineer, distributed, systems, development'
