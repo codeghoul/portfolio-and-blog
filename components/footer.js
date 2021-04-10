9f502a39610c12974afa853557ecb460691e74dc
codeghoul <jayesh.patel.2197@gmail.com>
2021-04-10 09:01:51 +0530

    Redesign Footer.

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,9 @@
-import Container from './container'
-
 export default function Footer() {
   return (
-    <footer className='bg-accent-1 border-t border-accent-2'>
-      <Container>
-        <p className='text-xl font-bold tracking-tight md:tracking-tighter leading-tight text-center mb-4 mt-4'>
-          Made with ❤ and ☕ by Jayesh Patel
-        </p>
-      </Container>
+    <footer className='py-4'>
+      <p className='text-xs md:text-sm dark:text-white font-sans tracking-tight md:tracking-tighter leading-tight text-center'>
+        Made with ❤ and ☕ by Jayesh Patel
+      </p>
     </footer>
   )
 }
