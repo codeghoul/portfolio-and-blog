eca1da4922e49ade6a895dc2912e9b90a089baad
codeghoul <jayesh.patel.2197@gmail.com>
2021-04-10 13:15:31 +0530

    Optimize Avatar Image.

--- components/avatar.js
+++ components/avatar.js
@@ -1,11 +1,11 @@
+import Image from 'next/image'
+
 export default function Avatar({ name, picture }) {
   return (
     <div className='flex items-center'>
-      <img
-        src={picture.url}
-        className='w-8 h-8 lg:w-12 lg:h-12 rounded-full mr-4'
-        alt={name}
-      />
+      <div className='w-8 h-8 lg:w-12 lg:h-12 rounded-full mr-2 md:mr-4'>
+        <Image src={picture.url} alt={name} width={48} height={48} />
+      </div>
       <div className='text-sm lg:text-xl font-bold'>{name}</div>
     </div>
   )
