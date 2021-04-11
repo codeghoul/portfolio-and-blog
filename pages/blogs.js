b431012c29a9408c1ef871a5c7bac4aa7f6eab39
codeghoul <jayesh.patel.2197@gmail.com>
2021-04-11 18:02:33 +0530

    Clean Up.

--- pages/blogs.js
+++ pages/blogs.js
@@ -9,29 +9,27 @@ export default function Blogs({ preview, allPosts }) {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1)
   return (
-    <>
-      <Layout preview={preview}>
-        <PageSeo
-          title={`Blogs by`}
-          description={siteMetadata.description}
-          url={`${siteMetadata.siteUrl}/blogs`}
-        />
-        {heroPost && (
-          <div className='my-4 md:my-6'>
-            <PostPreview
-              key={heroPost.slug}
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          </div>
-        )}
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-      </Layout>
-    </>
+    <Layout preview={preview}>
+      <PageSeo
+        title={`Blogs by`}
+        description={siteMetadata.description}
+        url={`${siteMetadata.siteUrl}/blogs`}
+      />
+      {heroPost && (
+        <div className='my-4 md:my-6'>
+          <PostPreview
+            key={heroPost.slug}
+            title={heroPost.title}
+            coverImage={heroPost.coverImage}
+            date={heroPost.date}
+            author={heroPost.author}
+            slug={heroPost.slug}
+            excerpt={heroPost.excerpt}
+          />
+        </div>
+      )}
+      {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+    </Layout>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -4,40 +4,38 @@ import { PageSeo } from '../components/seo'
 
 export default function HomePage() {
   return (
-    <>
-      <Layout preview={false}>
-        <PageSeo
-          title={siteMetadata.title}
-          description={siteMetadata.description}
-          url={siteMetadata.siteUrl}
-        />
-        <section className='text-gray-600 dark:text-gray-300'>
-          <div className='container mx-auto flex px-5 py-18 items-center justify-center flex-col'>
-            {/* <img
+    <Layout preview={false}>
+      <PageSeo
+        title={siteMetadata.title}
+        description={siteMetadata.description}
+        url={siteMetadata.siteUrl}
+      />
+      <section className='text-gray-600 dark:text-gray-300'>
+        <div className='container mx-auto flex px-5 py-18 items-center justify-center flex-col'>
+          {/* <img
               className='lg:w-2/6 md:w-3/6 w-5/6 object-cover object-center rounded'
               alt='hero'
               src='/images/myself.png'
             /> */}
-            <div className='text-center lg:w-2/3 w-full mt-20 md:mt-32 lg:mt-56'>
-              <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-gray-200'>
-                Hi! I'm Jayesh!
-              </h1>
-              <p className='mb-8 leading-relaxed'>
-                For the past 1 year, I’ve been working as a Backend Developer at
-                GlobalLogic Inc. I have a sound knowledge of Microservice
-                Architecture & Spring Framework is my weapon of choice though I
-                do have some background in Frontend Development and I love
-                dabbling in React.js & Gatsby during my free time.
-              </p>
-              <p className='mb-8 leading-relaxed'>
-                I’ve always been good at problem-solving & building new things.
-                Being a passionate learner, I look forward to expanding my
-                experience across different industries & technologies.
-              </p>
-            </div>
+          <div className='text-center lg:w-2/3 w-full mt-20 md:mt-32 lg:mt-56'>
+            <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-gray-200'>
+              Hi! I'm Jayesh!
+            </h1>
+            <p className='mb-8 leading-relaxed'>
+              For the past 1 year, I’ve been working as a Backend Developer at
+              GlobalLogic Inc. I have a sound knowledge of Microservice
+              Architecture & Spring Framework is my weapon of choice though I do
+              have some background in Frontend Development and I love dabbling
+              in React.js & Gatsby during my free time.
+            </p>
+            <p className='mb-8 leading-relaxed'>
+              I’ve always been good at problem-solving & building new things.
+              Being a passionate learner, I look forward to expanding my
+              experience across different industries & technologies.
+            </p>
           </div>
-        </section>
-      </Layout>
-    </>
+        </div>
+      </section>
+    </Layout>
   )
 }
