5d71f2e682ae02fe1781f7f78ee0043e89eabe50
codeghoul <jayesh.patel.2197@gmail.com>
2023-09-09 12:14:30 +0530

    Fix purge css.

--- postcss.config.js
+++ postcss.config.js
@@ -8,8 +8,8 @@ module.exports = {
         ? {
             // the paths to all template files
             content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}',
+              './src/pages/**/*.{js,jsx,ts,tsx}',
+              './src/components/**/*.{js,jsx,ts,tsx}',
             ],
             // function used to extract class names from the templates
             defaultExtractor: (content) =>
